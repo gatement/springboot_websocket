1d9ac0f98b9e6b4ae46a86a4309a42b0228c911d
Johnson Lau <gatement@gmail.com>
2018-05-26 07:29:01 +0800

    go

--- src/main/java/lgh/springboot/websocket/TestController.java
+++ src/main/java/lgh/springboot/websocket/TestController.java
@@ -3,11 +3,15 @@ package lgh.springboot.websocket;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import lombok.extern.slf4j.Slf4j;
+
 @RestController
+@Slf4j
 public class TestController {
 
 	@GetMapping("/hello")
 	public String hello() {
+		log.info("GET /hello");
 		return "[" + String.valueOf(System.currentTimeMillis()) + "] HelloWorld";
 	}
 
