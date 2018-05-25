183a619874a4f3baf5f2933b9f29cf8f33b08493
Johnson Lau <gatement@gmail.com>
2018-05-26 07:07:15 +0800

    go

---
+++ README.md
@@ -0,0 +1,3 @@
+a websocket demo
+
+reference: https://www.cnblogs.com/bianzy/p/5822426.html
---
+++ src/main/java/lgh/springboot/websocket/App.java
@@ -0,0 +1,12 @@
+package lgh.springboot.websocket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class App {
+
+	public static void main(String[] args) {
+		SpringApplication.run(App.class, args);
+	}
+}
